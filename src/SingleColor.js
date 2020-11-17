42ed8af369d638489c7ffad9d863bbf264c8d5c1
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-11-18 01:25:08 +0530

    changes done

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -4,15 +4,27 @@ import rgbToHex from "./utils";
 const SingleColor = ({ rgb, weight, index }) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(",");
+  const hexValue = rgbToHex(...rgb);
 
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => clearTimeout(timeOut);
+  }, [alert]);
   return (
     <>
       <article
         className={`color ${index > 10 && "color-light"}`}
+        onClick={() => {
+          setAlert(true);
+          navigator.clipboard.writeText(hexValue);
+        }}
         style={{ background: `rgb(${bcg})` }}
       >
         <p className="percent-value">{weight}</p>
-        <p className="color-value">{rgbToHex(...rgb)}</p>
+        <p className="color-value">{hexValue}</p>
+        {alert && <p className="alert">copied to clipboard</p>}
       </article>
     </>
   );
